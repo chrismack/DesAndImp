2407c69639b41d05e4524e3f1453e85088d27fa2
Chris Mack <chris.mack@hotmail.co.uk>
2015-11-03 23:39:32 +0000

    Outline for dynamic array .h and .cpp

---
+++ SeminarB/SeminarB/Main.cpp
@@ -0,0 +1,19 @@
+// Main.cpp : Defines the entry point for the console application.
+/*
+*Author : Chris Mack n0576565
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int main()
+{
+	cout << sizeof(int) << endl;
+	system("pause");
+    return 0;
+}
+
