f8caba3ea202f17ba42431c0b2fef6d444911dc4
Chris Mack <chris.mack@hotmail.co.uk>
2016-02-26 16:55:58 +0000

    implement missing methods

--- Seminars/SeminarD/DoubleDVD.cpp
+++ Seminars/SeminarD/DoubleDVD.cpp
@@ -23,4 +23,22 @@ std::tuple<int, int, int> DoubleDVD::getPackageSize() const
 	return this->packageDimensions_;
 }
 
+std::map<int, SDI::DynArray<std::string>> DoubleDVD::getAllContent() const
+{
+	return std::map<int, SDI::DynArray<std::string>>();
+}
+
+void DoubleDVD::setContent(const std::map<int, SDI::DynArray<std::string>> content)
+{
+}
+
+SDI::DynArray<std::string> DoubleDVD::getContentOnSide(const int side) const
+{
+	return SDI::DynArray<std::string>();
+}
+
+void DoubleDVD::setContentOnSide(const int, const SDI::DynArray<std::string> content)
+{
+}
+
 #endif // !DOUBLEDVD_CPP
--- Seminars/SeminarD/VHS.cpp
+++ Seminars/SeminarD/VHS.cpp
@@ -22,4 +22,13 @@ std::tuple<int, int, int> VHS::getPackageSize() const
 	return this->packageDimensions_;
 }
 
+std::string VHS::getLanguageTrack() const
+{
+	return std::string();
+}
+
+void VHS::setlanguageTrack(const std::string languageTrack)
+{
+}
+
 #endif // !VHS_CPP
