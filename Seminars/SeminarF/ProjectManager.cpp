e6a0a2e1976d35319d4a3c056367841b06cd2e7c
Chris Mack <chris.mack@hotmail.co.uk>
2016-04-14 17:33:52 +0100

    file creation

--- Seminars/SeminarF/ProjectManager.cpp
+++ Seminars/SeminarF/ProjectManager.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <time.h>
 
 #include "Logger.h"
 
@@ -30,6 +31,8 @@ ProjectManager::ProjectManager(SDI::Logger *logger)
 
 ProjectManager::~ProjectManager()
 {
+	projects_.clear();
+	materials_.clear();
 }
 
 /*
@@ -129,11 +132,28 @@ void ProjectManager::importCSV(const std::string &CSV)
 	materialsFromFile.clear();
 
 	/*
-	* Append projects to list of existing projects
-	*/
+	 * Append projects to list of existing projects
+	 */
 	std::vector<Project*> projectsFromFile = csvHandler.getProjectsFromFile();
-	projects_.insert(projects_.end(), projectsFromFile.begin(), projectsFromFile.end());	// Append to existing projects
-	projectsFromFile.clear();
+	for (Project* project : projectsFromFile)
+	{
+		std::cout << "Is this project now playing?" << std::endl;
+		std::cout << project->getTitle() << " : " << project->getGenre() << " : " << project->getSummary() << std::endl;
+		std::string input = "";
+		while (toLower(input) != "yes" || toLower(input) != "no")
+		{
+			std::cout << "please enter a valid option. Yes or No" << std::endl;
+			input = getUserInput();
+			if (toLower(input) == "yes" || toLower(input) == "no")
+			{
+				projects_.insert(std::pair<Project*, bool>(project, toLower(input) == "yes" ? true : false));
+			}
+		}
+	}
+	//projects_.insert(projects_.end(), projectsFromFile.begin(), projectsFromFile.end());	// Append to existing projects
+	//projectsFromFile.clear();
+
+	delete mf;
 }
 
 
@@ -154,25 +174,143 @@ const bool ProjectManager::fileExists(const std::string & path)
 }
 
 
+
 /*
  * Display menu options on screen
  */
 void ProjectManager::displayMenuOptions()
 {
-	std::cout << "			MENU			      "	<< std::endl;
+	std::cout << "			MENU			      " << std::endl;
 	std::cout << "0)exit   : leave the application" << std::endl;		// Leave applicatoin
-	std::cout << "1)import : Import from file"		<< std::endl;		// Import materials from files
-	std::cout << "2)save   : Save to file"			<< std::endl;		// Save material strings to file
-	std::cout << "3)view   : View data"				<< std::endl;		// View Materials and projects currently stored in memory
-	std::cout << std::endl;			
+	std::cout << "1)import : Import from file" << std::endl;		// Import materials from files
+	std::cout << "2)save   : Save to file" << std::endl;		// Save material strings to file
+	std::cout << "3)view   : View data" << std::endl;		// View Materials and projects currently stored in memory
+	std::cout << "4)create : Create Material or Proj" << std::endl;
+	std::cout << std::endl;
 }
 
+/*
+ * Display options for viewing materials loaded into memory
+ */
 void ProjectManager::displayViewOptions()
 {
 	std::cout << "What would you like to view?" << std::endl << "1)Projects or 2)Materials" << std::endl;;
 	std::cout << "Please enter you choice" << std::endl;
+	processViewOptinos();
 }
 
+void ProjectManager::processViewOptinos()
+{
+	std::string viewOption = toLower(getUserInput());
+	if (viewOption == "1" || viewOption == "projects")
+	{
+
+		std::map<Project*, bool>::iterator it;
+		for (it = projects_.begin(); it != projects_.end(); ++it)
+		{
+			std::cout << it->first->getTitle() << std::endl;
+		}
+		/*for (Project* project : projects_)
+		{
+			std::cout << project->getTitle() << std::endl;
+			delete project;
+		}*/
+	}
+	else if (viewOption == "2" || viewOption == "materials")
+	{
+		for (Material* material : materials_)
+		{
+			std::cout << material->getId() << " : " << material->getFilmTitle() << std::endl;
+			delete material;
+		}
+	}
+	else		// No valid option has been entered for view option
+	{
+		std::cout << "You didn't enter a valid option for view" << std::endl;
+	}
+}
+
+/*
+ * display the options for creating materials or projects
+ */
+void ProjectManager::displayCreateMenu()
+{
+	std::cout << "What operation would you like to complete?" << std::endl;
+	std::cout << "1) Create Project" << std::endl;		// Create a project and add to projects_
+	std::cout << "2) Create Material" << std::endl;		// Create a material and add to materials_
+	std::cout << "3) Link Media" << std::endl;		// Display linking menu
+
+	std::cout << "Please enter your option : " << std::endl;
+	processCreateOptions();
+}
+
+void ProjectManager::processCreateOptions()
+{
+	std::string input = toLower(getUserInput());
+	if (input == "1" || input == "create project")
+	{
+		Project* project = createProject();
+
+		std::cout << "Is the project now playing?" << std::endl;
+
+		std::string input = "";
+		while (toLower(input) != "yes" && toLower(input) != "no")
+		{
+			std::cout << "please enter a valid option. Yes or No" << std::endl;
+			input = getUserInput();
+			if (toLower(input) == "yes" || toLower(input) == "no")
+			{
+				projects_.insert(std::pair<Project*, bool>(project, toLower(input) == "yes" ? true : false));
+			}
+		}
+	}
+	else if (input == "2" || input == "create material")
+	{
+		createMaterial();
+	}
+	else if (input == "3" || input == "link media")
+	{
+		displayLinkingMenu();
+	}
+}
+
+Project * ProjectManager::createProject()
+{
+	Project* project = new Project();
+	// title, summary, genre, releasedate, filminglocation, language, keywords, ticketSale
+	project->setTitle(messageReturnInput("Set Title"));
+	project->setSummary(messageReturnInput("Set Summary"));
+	project->setGenre(messageReturnInput("Set Genre"));
+	project->setReleaseDate(messageGetDate("Set release date"));
+	project->setFilmingLocations(messageReturnUserVector("Set Filming Locations"));
+	project->setLanguage(messageReturnInput("Set Language"));
+	project->setKeywords(messageReturnUserVector("Set keywords"));
+
+	return project;
+}
+
+Material * ProjectManager::createMaterial()
+{
+	return nullptr;
+}
+
+/*
+ * Display options for linking materials and projects together
+ */
+void ProjectManager::displayLinkingMenu()
+{
+	std::cout << "Would you like to link a Project to a material or material to project" << std::endl;
+	std::cout << "1) Project to material" << std::endl;
+	std::cout << "2) Material to project" << std::endl;
+}
+
+void ProjectManager::processLinkingOptins()
+{
+}
+
+/*
+ * Has the user finished using the program
+ */
 void ProjectManager::userContinueOption()
 {
 
@@ -219,13 +357,20 @@ std::string ProjectManager::getUserInput()
 	 {
 		 std::ofstream saveFile(defaultSavePath);
 		 saveFile.open(defaultSavePath, std::ofstream::out);
-		 CSVHandler csvHandler(defaultSavePath, new MaterialFactory());
-		 
-		 for (Project* project : projects_)
+		 MaterialFactory* mf = new MaterialFactory();
+		 CSVHandler csvHandler(defaultSavePath, mf);
+
+		 std::map<Project*, bool>::iterator it;
+		 for (it = projects_.begin(); it != projects_.end(); ++it)
+		 {
+			 csvHandler.writeToFile(it->first);
+		 }
+
+		 /*for (Project* project : projects_)
 		 {
 			 csvHandler.writeToFile(project);
 			 delete project;
-		 }
+		 }*/
 
 		 for (Material* material : materials_)
 		 {
@@ -234,31 +379,15 @@ std::string ProjectManager::getUserInput()
 		 }
 
 		 saveFile.close();
+		 delete mf;
 	 }
 	 else if (input == "3" || input == "view")
 	 {
 		 displayViewOptions();
-		 std::string viewOption = toLower(getUserInput());
-		 if (viewOption == "1" || viewOption == "projects")
-		 {
-			 for (Project* project : projects_)
-			 {
-				 std::cout << project->getTitle() << std::endl;
-				 delete project;
-			 }
-		 }
-		 else if (viewOption == "2" || viewOption == "materials")
-		 {
-			 for (Material* material : materials_)
-			 {
-				 std::cout << material->getId() << " : "<< material->getFilmTitle() << std::endl;
-				 delete material;
-			 }
-		 }
-		 else		// No valid option has been entered for view option
-		 {
-			 std::cout << "You didn't enter a valid option for view" << std::endl;
-		 }
+	 }
+	 else if (input == "4" || input == "create")
+	 {
+		 displayCreateMenu();
 	 }
 	 else			// No valid option has been entered
 	 {
@@ -267,6 +396,140 @@ std::string ProjectManager::getUserInput()
  }
 
  /*
+  * display user with a message and return a non null input
+  */
+const std::string ProjectManager::messageReturnInput(const std::string & message)
+{
+	std::cout << message << std::endl;
+	std::cout << "Please enter your input" << std::endl;
+
+	std::string input = getUserInput();
+	while ((input == "") && (input.find_first_of(" ") == std::string::npos))  // Ensure user input is not null or only spaces
+	{
+		std::cout << "Your input cannot be nothing. Please try again" << std::endl;
+		input = getUserInput();
+	}
+
+	return input;
+}
+
+/*
+ * Heavily based on:
+ * http://www.cplusplus.com/reference/ctime/mktime/
+ */
+const long ProjectManager::messageGetDate(const std::string & message)
+{
+
+	std::cout << message << std::endl;
+
+	time_t rawTime;
+	struct tm timeInfo;
+	int year, month, day;
+	long timestamp = -1;
+
+	bool validInput = true;
+
+	while (timestamp == -1)
+	{
+		/*
+		 * Get user input date
+		 */
+		try
+		{
+			std::cout << "Enter Year: " ; year  = stoi(getUserInput());
+			std::cout << "Enter Month: "; month = stoi(getUserInput());
+			std::cout << "Enter Day: "  ; day   = stoi(getUserInput());
+			//printf("Enter year: "); fflush(stdout); scanf_s("%d", &year);
+			//printf("Enter month: "); fflush(stdout); scanf_s("%d", &month);
+			//printf("Enter day: "); fflush(stdout); scanf_s("%d", &day);
+		}
+		catch (std::invalid_argument ia)
+		{
+			validInput = false;
+			std::cout << "Invalid input please re enter" << std::endl;
+		}
+
+		if (validInput)
+		{
+			time(&rawTime);						
+			localtime_s(&timeInfo, &rawTime);				// Init timeInfo struct
+	
+			timeInfo.tm_year = year - 1900;					// Setup timeInfo struct
+			timeInfo.tm_mon = month - 1;
+			timeInfo.tm_mday = day;
+			timeInfo.tm_hour = 1;
+			timeInfo.tm_min = 0;
+			timeInfo.tm_sec = 0;
+		
+			timestamp = mktime(&timeInfo);					// COnvert to unix
+		
+			if (timestamp == -1)							// Invalid date set entered
+				std::cout << "invalid date please re enter" << std::endl;
+		}
+	}
+
+	return timestamp;
+}
+
+const std::vector<std::string> ProjectManager::messageReturnUserVector(const std::string & message)
+{
+	std::cout << message << std::endl;
+	
+	std::cout << "Atleast one value needs to be entered" << std::endl;
+	std::cout << "enter : exit---loop to finish" << std::endl;
+	
+	std::vector<std::string> userData = {};
+	std::string input = "";
+	
+	while ((toLower(input) != "exit---loop") || userData.empty())
+	{
+		std::cout << "Please enter a value. Or exit---loop to finish" << std::endl;
+		input = getUserInput();
+		if (input != "" && input.find_first_of(" ") == std::string::npos && toLower(input) != "exit---loop")
+		{
+			userData.push_back(input);
+		}
+	}
+
+	return userData;
+}
+
+
+const std::vector<int> ProjectManager::messageReturnUserVectorInt(const std::string & message)
+{
+
+	std::cout << message << std::endl;
+
+	std::cout << "Atleast one value needs to be entered" << std::endl;
+	std::cout << "enter : exit---loop to finish" << std::endl;
+
+	std::vector<int> userData = {};
+	std::string input = "";
+
+	while ((toLower(input) != "exit---loop") || userData.empty())
+	{
+		std::cout << "Please enter a value. Or exit---loop to finish" << std::endl;
+		std::cout << "This is data set : " << userData.size() + 1 << std::endl;
+		input = getUserInput();
+		if (toLower(input) != "exit---loop")
+		{
+			try
+			{
+				userData.push_back(std::stoi(input));
+			}
+			catch (std::invalid_argument ia)
+			{
+				std::cout << "Could not convert " << input << " to an integer" << std::endl;
+			}
+		}
+	}
+
+
+	return userData;
+}
+
+
+/*
   * Converts the input string to all lower case characters
   */
  std::string ProjectManager::toLower(std::string & input)
--- Seminars/SeminarF/ProjectManager.h
+++ Seminars/SeminarF/ProjectManager.h
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 #include "Material.h"
 #include "Project.h"
@@ -21,13 +22,13 @@ private: // Variables
 	/*
 	 * List of all imported projects
 	 */
-	std::vector<Project*>  projects_;
+	//std::vector<Project*>  projects_;
+	std::map<Project*, bool> projects_;
 
 	/*
 	 * List of all imported materials
 	 */
 	std::vector<Material*> materials_;
-
 	/*
 	 * Flag to test if the user wants to leave the application
 	 */
@@ -74,6 +75,45 @@ private: // Functions
 	void displayViewOptions();
 
 	/*
+	 * Process the user input for the view menu
+	 */
+	void processViewOptinos();
+
+	/*
+	 * Display the create menu sub options
+	 * displayed when option 4(create) from menu is selected
+	 */
+	void displayCreateMenu();
+
+	/*
+	 * Process the user input for the create media options
+	 */
+	void processCreateOptions();
+
+	/*
+	 * Allow user to create a material
+	 * Should allow materials with the same name as existing material
+	 */
+	Project* createProject();
+
+	/*
+	 * Allow user to create a material
+	 * Present options depending on material type
+	 */
+	Material* createMaterial();
+
+	/*
+	 * Display the options for linking media together
+	 * Material to Project or Project to Material
+	 */
+	void displayLinkingMenu();
+
+	/*
+	 * Process the user input for linking media together
+	 */
+	void processLinkingOptins();
+
+	/*
 	 * Display a message to the user and ask if they would like to continue
 	 * Process the information if they would like to carry one in the application or not
 	 */
@@ -109,6 +149,33 @@ private: // Functions
 	 * Check if file at path exists
 	 */
 	const bool fileExists(const std::string &path);
+
+	/*
+	 * Display a message and get a non null user input
+	 */
+	const std::string messageReturnInput(const std::string& message);
+
+	/*
+	 * Display a message and convert the user input into a unix time stamp
+	 */
+	const long messageGetDate(const std::string& message);
+
+	/*
+	 * Display a message and continue to get user inputs and add to a vector until the user is satisifed
+	 */
+	const std::vector<std::string> messageReturnUserVector(const std::string& message);
+
+	/*
+	 * Display a message and continue to get user inputs and add to a vector until the user is satisifed
+	 * returns vector of ints, used for ticket sales
+	 */
+	const std::vector<int> messageReturnUserVectorInt(const std::string& message);
+
+	/*
+	 * This should be in the CSVHandler but i'm not sure if i'm aloud to chage it so it shall go here
+	 * Write Projects to file with the now playing arg
+	 */
+	void writeToFile(Project* project, bool nowPlaying);
 
 	/*
 	 * Converts a string to all lower case
