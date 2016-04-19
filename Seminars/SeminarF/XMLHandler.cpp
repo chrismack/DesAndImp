65e9a02476f1e61686dac90249b6cacf6ad177aa
chrismack <chris.mack@hotmail.co.uk>
2016-04-19 13:07:04 +0100

    Start xml handler

--- Seminars/SeminarF/XMLHandler.cpp
+++ Seminars/SeminarF/XMLHandler.cpp
@@ -2,11 +2,40 @@
 #include "XMLHandler.h"
 
 
-XMLHandler::XMLHandler()
+using namespace tinyxml2;
+
+XMLHandler::XMLHandler(const char * fileName, MaterialFactory* materialFactory)
 {
+
+	this->fileName = fileName;
+	this->materialfactory = materialFactory;
+
+	XMLDocument document;
+	document.Parse(fileName);
+	XMLElement* titleElement = document.FirstChildElement("poetry")->FirstChildElement("verse");
 }
 
 
 XMLHandler::~XMLHandler()
 {
+}
+
+std::map<Project*, bool> XMLHandler::getProjects()
+{
+	return projects_;
+}
+
+std::map<Project*, bool> XMLHandler::getProjectsFromFile()
+{
+	return projects_;
+}
+
+std::vector<Material*> XMLHandler::getMaterials()
+{
+	return this->materials_;
+}
+
+std::vector<Material*> XMLHandler::getMaterialsFromFile()
+{
+	return this->materials_;
 }
--- Seminars/SeminarF/XMLHandler.h
+++ Seminars/SeminarF/XMLHandler.h
@@ -1,8 +1,30 @@
 #pragma once
+#include "tinyxml2.h"
+
+#include "MaterialFactory.h"
+#include "Project.h"
+#include "Material.h"
+
+#include <string>
+#include <map>
+
 class XMLHandler
 {
+private:
+	const char * fileName;
+	MaterialFactory* materialfactory;
+
+	std::map<Project*, bool> projects_;
+	std::vector<Material*> materials_;
+
 public:
-	XMLHandler();
+	XMLHandler(const char * filename, MaterialFactory* materialFactory);
 	~XMLHandler();
+
+	std::vector<Material*> getMaterialsFromFile();
+	std::map<Project*, bool> getProjectsFromFile();
+
+	std::vector<Material*> getMaterials();
+	std::map<Project*, bool> getProjects();
 };
 
